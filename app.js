1e68f941ac82e019187590d25ea8f41854c5806c
JuanCarlini <juanandrescarlini@gmail.com>
2021-08-03 14:31:42 -0300

    estructura de archivos

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Hola");
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const publicPath = path.resolve(__dirname, "./public");
+app.use(express.static(publicPath));
+
+app.listen(3000, () =>{
+    console.log("Servidor corriendo en 3000");
+})
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+})
