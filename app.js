e3d4896d7bbd2258e041e6d8843d5d640a67653f
lauutarol <lopez.lautaro1997@gmail.com>
2021-08-24 17:35:54 -0300

    Corrijo errores  EJS

--- app.js
+++ app.js
@@ -12,13 +12,7 @@ app.set('view engine', 'ejs');
 
 app.use('/', mainRoutes)
 
-app.use('/log-in', mainRoutes)
 
-app.use('/register', mainRoutes)
-
-app.use('/carrito', mainRoutes)
-
-app.use('/detalle-del-producto', mainRoutes)
 
 app.listen(3000, () =>{
     console.log("Servidor corriendo en 3000");
