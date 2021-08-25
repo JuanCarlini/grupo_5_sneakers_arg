2e4697fa98a78ff1758e4691563413f5c4a5515b
JuanCarlini <juanandrescarlini@gmail.com>
2021-08-24 21:51:31 -0300

    prueba

--- app.js
+++ app.js
@@ -1,40 +1,11 @@
 const express = require("express");
-<<<<<<< HEAD
-const react = require('react')
-=======
 
->>>>>>> e3d4896d7bbd2258e041e6d8843d5d640a67653f
 const app = express();
 
 const mainRoutes = require('./routers/main')
 
 app.use(express.static('./public')); 
 
-<<<<<<< HEAD
-react.get("/", (req, res) => {
-    render(path.resolve(__dirname, "./views/home.html"))
-})
-
-react.post("/", (req, res) => {
-    render(path.resolve(__dirname, "./views/home.html"))
-})
-
-react.get("/log-in", (req, res) => {
-    render(path.resolve(__dirname, "./views/log-in.html"))
-})
-
-react.get("/register", (req, res) => {
-    render(path.resolve(__dirname, "./views/register.html"))
-})
-
-react.get("/carrito", (req, res) => {
-    render(path.resolve(__dirname, "./views/carrito.html"))
-})
-
-react.get("/detalle-del-producto", (req, res) => {
-    render(path.resolve(__dirname, "./views/detalle-del-producto.html"))
-})
-=======
 app.set('view engine', 'ejs');
 
 
@@ -48,4 +19,3 @@ app.listen(3000, () =>{
 })
 
 
->>>>>>> e3d4896d7bbd2258e041e6d8843d5d640a67653f
