320663f4c9cb4b99fda6a8c7d1b3b0cea5bab7dd
lauutarol <lautaro.lopez1997@gmail.com>
2021-10-25 21:22:30 -0300

    Login2

--- models/User.js
+++ models/User.js
@@ -2,14 +2,31 @@ const fs = require('fs');
 
 const User ={
     filename: './database/users.json',
-    getData: function (){
-        return fs.readFileSync(this.filename, 'utf-8');
+
+    getData: function () {
+        return JSON.parse(fs.readFileSync(this.filename, 'utf-8'));
     },
+
+    findAll: function () {
+        return this.getData();
+    },
+
+    findByPk: function (id) {
+        let allUsers = this.findAll();
+        let userFound = allUsers.find(oneUser => oneUser.id === id);
+        return userFound;
+    },
+
+    findByField: function (field, text) {
+        let allUsers = this.findAll();
+        let userFound = allUsers.find(oneUser => oneUser[field] === text);
+        return userFound;
+    },
+
     create: function (userData){
 
     }
 }
 
-console.log(User.getData());
+console.log(User.findByField('email', 'juan@gmail.com'));
 
-// Verificar el punto
