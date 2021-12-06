2fee66a7f3370301cd3f845e8c6df7cccc83ea80
JuanCarlini <juanandrescarlini@gmail.com>
2021-12-06 12:10:26 -0300

    relaciones

--- database/models/Usuario.js
+++ database/models/Usuario.js
@@ -74,13 +74,17 @@ module.exports= (sequelize,DataTypes) =>{
             timestamps:false}
     );
 
-   /*  usuario.belongsToMany(models.producto, {
-        as: "producto",
+   
+   usuario.assosiate = function(models){
+       usuario.belongsToMany(models.producto, {
+        as: "productos",
         through: "usuarios_productos",
         foreignKey: "usuarios_id",
         other: "productos_id",
         timestamps: false
-    }) */
+    })
+   }
+    
     
 
     return usuario
